534849e3cec26365131accc6444a8b98af910c68
Leo <chris@chris-macleod.ca>
2021-01-18 13:16:31 -0500

    Use Image::at, add the rest of the diagonal calculations
    This is 6 month old work that that I apparently just never committed for some reason.

--- src/AIS_cubic.hpp
+++ src/AIS_cubic.hpp
@@ -9,40 +9,6 @@
 
 using namespace gsl;
 
-Image AIS_cubic(const Image& src) {
-	// Create a new image that's twice the size of the original
-	// minus the last column and row
-	const unsigned int width  = src.dimensions().width * 2 - 1;
-	const unsigned int height = src.dimensions().height * 2 - 1;
-	Image              dst({width, height}, src.channels());
-
-	// Copy pixels from the source to the destination
-	for(index x = 0; x < width; x += 2) {
-		for(index y = 0; y < height; y += 2) {
-			for(index channel = 0; channel < dst.channels(); ++channel) {
-				dst.set(x, y, src.at(x / 2, y / 2, channel), channel);
-			}
-		}
-	}
-
-	// Fill the interior pixels
-	for(index x = 1; x < width; x += 2) {
-		for(index y = 1; y < height; y += 2) {
-			for(index channel = 0; channel < dst.channels(); ++channel) {
-				// Find the value with solve_interior
-				// TODO: Bounds checking
-				// TODO: solve_interior needs to be adjusted to take channels into account
-			}
-		}
-	}
-
-	// Fill the aligned pixels
-	/*In the second stage, the pixels Y 2i,2j+1 and Y 2i+1,2j can be interpolated
-	similarly with the interpolation window slanted at 45 diagonal direction.
-	Interpolation of these pixels is performed with the aid of the interpolated
-	pixels in the first stage.*/
-}
-
 int G1(const Image& src, int i, int j, int channel) {
 	int accumulator = 0;
 
@@ -130,11 +96,11 @@ int LD(const Image& src, int i, int j, int channel) {
 }
 
 uint8_t solve_interior(const Image& src, int x, int y, int channel) {
-	// TODO: Modify to use Image.at(), inline into AIS_cubic()\
+	// TODO: Inline into AIS_cubic()
+	// TODO: Create mapping for stage 2
 	// Formulate the gradient calculation as a loop and break it out into its
 	// own function to limit the nesting. Loops are trivially parallelizable
 
-	// TODO: Calc gradients (using functions)
 	int gradient_1 = G1(src, y, x, channel);
 	int gradient_2 = G2(src, y, x, channel);
 
@@ -153,22 +119,56 @@ uint8_t solve_interior(const Image& src, int x, int y, int channel) {
 		       (1.0 / 16.0) * src.at(x + 3, y - 3, channel);
 	} else {
 		// Non-strong edge
-		int gradient__left_up =
-		  d(28, 44) + d(14, 30) + d(0, 16) + d(2, 18) + d(4, 20);
-		int gradient__right_up = RU(src, y, x, channel);
-		int gradient__left_down =
-		  d(14, 2) + d(28, 16) + d(42, 30) + d(44, 32) + d(46, 34);
-		int gradient__right_down =
-		  d(4, 20) + d(18, 34) + d(32, 48) + d(30, 46) + d(28, 44);
+		int gradient__left_up    = LU(src, y, x, channel);
+		int gradient__right_up   = RU(src, y, x, channel);
+		int gradient__left_down  = LD(src, y, x, channel);
+		int gradient__right_down = RD(src, y, x, channel);
 
 		double       weight_1   = 0.25 / (gradient__left_up + 1);
 		double       weight_2   = 0.25 / (gradient__right_up + 1);
 		double       weight_3   = 0.25 / (gradient__left_down + 1);
 		double       weight_4   = 0.25 / (gradient__right_down + 1);
 		const double weight_sum = weight_1 + weight_2 + weight_3 + weight_4;
-		return (weight_1 / weight_sum) * block[16] +
-		       (weight_2 / weight_sum) * block[18] +
-		       (weight_3 / weight_sum) * block[30] +
-		       (weight_4 / weight_sum) * block[32];
+		return (weight_1 / weight_sum) * src.at(x - 1, y - 1, channel) +
+		       (weight_2 / weight_sum) * src.at(x + 1, y - 1, channel) +
+		       (weight_3 / weight_sum) * src.at(x - 1, y + 1, channel) +
+		       (weight_4 / weight_sum) * src.at(x + 1, y + 1, channel);
 	}
+}
+
+Image AIS_cubic(const Image& src) {
+	// Create a new image that's twice the size of the original
+	// minus the last column and row
+	const unsigned int width  = src.dimensions().width * 2 - 1;
+	const unsigned int height = src.dimensions().height * 2 - 1;
+	Image              dst({width, height}, src.channels());
+
+	// Copy pixels from the source to the destination
+	for(index x = 0; x < width; x += 2) {
+		for(index y = 0; y < height; y += 2) {
+			for(index channel = 0; channel < dst.channels(); ++channel) {
+				dst.set(x, y, src.at(x / 2, y / 2, channel), channel);
+			}
+		}
+	}
+
+	// Fill the interior pixels
+	for(index x = 1; x < width; x += 2) {
+		for(index y = 1; y < height; y += 2) {
+			for(index channel = 0; channel < dst.channels(); ++channel) {
+				// Find the value with solve_interior
+				uint8_t value = solve_interior(src, x, y, channel);
+				dst.set(x, y, value, channel);
+				// TODO: Bounds checking
+			}
+		}
+	}
+
+	// Fill the aligned pixels
+	/*In the second stage, the pixels Y 2i,2j+1 and Y 2i+1,2j can be interpolated
+	similarly with the interpolation window slanted at 45 diagonal direction.
+	Interpolation of these pixels is performed with the aid of the interpolated
+	pixels in the first stage.*/
+
+	return dst;
 }
